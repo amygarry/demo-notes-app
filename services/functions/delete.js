41fa0d24bd51b9d07759879aff7394ece0c66008
Amy Garry <amy.garry@YS1031.local>
2023-02-21 14:21:57 -0700

    adding auth

--- services/functions/delete.js
+++ services/functions/delete.js
@@ -7,7 +7,7 @@ export const main = handler (async (event)=>{
         //'key' defines the partition key and sort key fo the item to be removed 
         Key: {
             userId: "123", //the id of the author 
-            noteId: event.pathPareameters.id, //the id of the note from the path
+            noteId: event.pathParameters.id, //the id of the note from the path
         },
     }
 
--- services/functions/update.js
+++ services/functions/update.js
@@ -8,14 +8,14 @@ export const main = handler (async (event)=>{
         //'Key' defines the partition key and sort key of the item to be updated 
         Key :{
             userId: "123", //the id of the author 
-            noteId: event.pathParmeters.id, //the id of the note from the path
+            noteId: event.pathParameters.id, //the id of the note from the path
         },
         //'UpdateExpression' defines the attributes to be updated 
         //'ExpressionAttributeValues' defines the value in the update expression 
         UpdateExpression: "SET content = :content, attachment = :attachment",
         ExpressionAttributeValues: {
-            "attachment": data.attachment || null,
-            "content": data.content || null,
+            ":attachment": data.attachment || null,
+            ":content": data.content || null,
         },
         //"ReturnValues" specifies if and how to return the items attributes, 
         // where ALL_NEW reutrns all attributes of the item after the update you can inspect 'results below to see how it works with different settings 
