5b72b5723235aa203c4a884a1bc9cf2bab556ce0
Amy Garry <amy.garry@YS1031.local>
2023-02-21 16:46:26 -0700

    added testing

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -21,7 +21,8 @@ export function ApiStack({ stack, app }) {
       "GET /notes/{id}": "functions/get.main",
       "GET /notes": "functions/list.main", 
       "PUT /notes/{id}": "functions/update.main",
-      "DELETE /notes/{id}": "functions/delete.main"
+      "DELETE /notes/{id}": "functions/delete.main",
+      "POST /billing": "functions/billing.main"
     },
   });
 
