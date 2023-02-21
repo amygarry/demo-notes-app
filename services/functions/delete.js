06adc277d0286e8a8348a868e457e98eeb1913b4
Amy Garry <amy.garry@YS1031.local>
2023-02-21 12:54:38 -0700

    added delete

---
+++ services/functions/delete.js
@@ -0,0 +1,16 @@
+import handler from "../util/handler"
+import dynamodb from "../util/dynamodb"
+
+export const main = handler (async (event)=>{
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        //'key' defines the partition key and sort key fo the item to be removed 
+        Key: {
+            userId: "123", //the id of the author 
+            noteId: event.pathPareameters.id, //the id of the note from the path
+        },
+    }
+
+    await dynamodb.delete(params)
+    return { status: true}
+})
