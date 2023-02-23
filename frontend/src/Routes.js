44e667175dc7a5200e7c2ff8456172fcde008652
Amy Garry <amy.garry@YS1031.local>
2023-02-23 13:25:26 -0700

    added new note connected to aws

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home"
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 
 export default function Links (){
     return (
@@ -10,6 +12,8 @@ export default function Links (){
             <Route path="/" element={<Home/>} />
             <Route path="*" element={<NotFound />} />;
             <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/notes/new" element={<NewNote />} />
         </Routes>
     )
 }
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,43 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Login.css"
+import { useNavigate } from "react-router-dom";
+import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../lib/contextLib";
+import { useFormFields } from "../lib/hooksLib";
+import { onError } from "../lib/errorLib";
+import "./Login.css";
 
-export default function Login(){
-    const [email, setEmail]=useState("");
-    const [password, setPassword]=useState("");
+export default function Login() {
+  const nav = useNavigate();
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+  });
 
-    function validateForm( ){
-        return email.length > 0 && setPassword.length >0
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(fields.email, fields.password);
+      userHasAuthenticated(true);
+      nav("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
     }
+  }
 
-    function handleSubmit (event){
-        event.preventDefault()
-    }
-
-    return (
-        <div className="Login">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control 
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={(e)=> setEmail(e.target.value)}/>
-                </Form.Group>
-                <Form.Group size="lg" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-        <Button block="true" size="lg" type="submit" disabled={!validateForm()}>
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <LoaderButton
+          block="true"
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
-        </div>
-    )
+    </div>
+  );
 }
